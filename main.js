24099ed6cc60f41707823c8ee23b097a256c0987
Asadbek <ibodullaevasadbek2003@gmail.com>
2024-01-17 12:05:25 +0500

    userinterface is done

--- main.js
+++ main.js
@@ -1,29 +1,2 @@
-function salom(name){
-    alert('Assalomu alaykum ' + name);
-}
-function shart(){
-    let baho = 5 ;
-    if (baho === 5) {
-        console.log('a`lo baho');
-        
-    }else{
-        console.log('Baholar a`lo emas');
-    }
-}
 
-
-
-function sikl() {
-    for (let i = 0; i <= 10; i++) {
-        console.log(i);
-        
-    }
-    
-}
-
-sikl();
-
-function addClass(argument){
-    document.getElementById('btn').classList.add('button');
-}
 
